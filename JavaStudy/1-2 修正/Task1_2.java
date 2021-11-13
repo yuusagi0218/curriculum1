b0a62036c5a5ee21ccd919e4114c6640d634a78d
yuusagi0218 <91358376+yuusagi0218@users.noreply.github.com>
2021-11-13 20:35:55 +0900

    課題提出　修正

---
+++ JavaStudy/1-2 修正/Task1_2.java
@@ -0,0 +1,28 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+            String[] country = {"JAPAN", "AMERICA", "KOREA"};
+            
+        // ② ①で作成した配列の要素数を出力してください。。
+            System.out.println("配列countryの要素数…" + country.length);
+        /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+         *   1番目（先頭）の要素に 「りんご」 を代入
+         *   2番目の要素に 「もも」 を代入
+         *   3番目の要素に 「ぶどう」 を代入
+         */
+            String[] strArray = {"りんご", "もも", "ぶどう"};
+        // ④ ③で作成した配列の2番目の要素を出力しなさい。
+            System.out.println(strArray[1]);
+        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+         *  [ここへ記述]10、20、30、40、50 を要素の値（初期化）とする、要素数5のintArrayという名前のint型配列を宣言。
+         */
+        int[] intArray = { 10, 20, 30, 40, 50 };
+            
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // [ここへ記述]添え字（インデックス） の要素の値 20 と 50 を足した合計 70 を表示
+        System.out.println(intArray[1] + intArray[4]);
+
+     }
+}
